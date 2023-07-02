f0101e400d2dff4f007e9b125abd0697cab720bb
alaakouki <119391046+alaakouki@users.noreply.github.com>
2023-07-02 13:02:09 -0700

    server files checked and next step SearchBooks.js

--- server/server.js
+++ server/server.js
@@ -4,10 +4,24 @@ const db = require('./config/connection');
 const routes = require('./routes');
 
 const { ApolloServer } = require("apollo-server-express");
-const { typeDefs, resolvers } = require("../server/schemas");
+const { typeDefs, resolvers } = require("./schemas");
+const {authMiddleware } = require("./utils/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
+
+// To apply Apollo server to Express
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+server.applyMiddleware({app});
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,6 +32,11 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(routes);
+
+// To get all
+app.get( "*", (req, res) => {
+res.sendFile(path.join(__dirname, "../client/public/index.html"));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
